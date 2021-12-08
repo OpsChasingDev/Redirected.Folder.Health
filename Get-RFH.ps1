cad4d16283c4662700631837482a7a2319333649
drummermanrob20 <robert@stapleton-family.net>
2021-12-07 20:25:32 -0500

    added comment to $Easteregg parameter

--- Get-RFH.ps1
+++ Get-RFH.ps1
@@ -13,7 +13,7 @@
         [string]$LogError,
 
         [switch]$ShowHost = $false,
-        [switch]$EasterEgg = $false
+        [switch]$EasterEgg = $false #but with a twist
     )
     
     BEGIN {
