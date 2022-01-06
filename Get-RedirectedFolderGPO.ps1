3b45dcd321afb16b31b9852dda20cfd59f2963ab
drummermanrob20 <robert@stapleton-family.net>
2022-01-05 20:02:19 -0500

    started object members

--- Get-RedirectedFolderGPO.ps1
+++ Get-RedirectedFolderGPO.ps1
@@ -1,4 +1,3 @@
-break
 # find best method to parse XML reports of the returned findings for which libraries they deal with and what their paths are
 # the above parsed values need to be added as members to the object stored in $RedirectionGPO
 # output
@@ -14,17 +13,15 @@ break
             # Name
             # Letter
             # Path
-
-(Get-GPOReport -All -ReportType xml).count
-
+<#
 [xml]$gpo = Get-GPO -Name 'RedirectedFolders_New01' | Get-GPOReport -ReportType xml
-
 # basic info
 $gpo.GPO
 # enabled and enforced (Enabled and NoOverride, respectively)
 $gpo.GPO.LinksTo
 # returns 'Folder Redirection' if handling folder redirection settings
 $gpo.GPO.User.ExtensionData.Name
+#>
 
 # grabs all GPOs, gets an XML report of their config, and returns GPOs that have folder redirection settings
 $AllGPO = Get-GPO -All
@@ -32,7 +29,18 @@ $AllGuid = $AllGPO.Id.Guid
 ForEach ($Guid in $AllGuid) {
     [xml]$Report = Get-GPOReport -Guid $Guid -ReportType xml
     If ($Report.GPO.User.ExtensionData.Name -contains 'Folder Redirection'){
+
+        # initial object creation and member assignments
         $RedirectionGPO = Get-GPO -Guid $Guid
-        Write-Output $RedirectionGPO
+        $obj = New-Object -TypeName psobject
+        $obj | Add-Member -Name 'DisplayName' -MemberType NoteProperty -Value $RedirectionGPO.DisplayName
+        $obj | Add-Member -Name 'GUID' -MemberType NoteProperty -Value $RedirectionGPO.Id
+
+        # determine the libraries in the GPO
+        $obj | Add-Member -Name 'Enabled' -MemberType NoteProperty -Value $Report.GPO.LinksTo.Enabled
+        $obj | Add-Member -Name 'Enforced' -MemberType NoteProperty -Value $Report.GPO.LinksTo.NoOverride
+        $obj | Add-Member -Name 'Desktop' -MemberType NoteProperty -Value $Report.GPO.User.ExtensionData.Extension.Folder.Location.DestinationPath
+
+        Write-Output $obj
     }
 }
