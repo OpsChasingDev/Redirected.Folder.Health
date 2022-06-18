1ab689d843481cf43f575de8608501a266a51caa
drummermanrob20 <robert@stapleton-family.net>
2022-06-18 10:04:23 -0400

    cleaned up alias

--- scratch.ps1
+++ scratch.ps1
@@ -2,7 +2,7 @@
 # Get-ChildItem 'REGISTRY::HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\ProfileList\' | Select Name
 
 # returns the username (SamAccountName) and corresponding SID for each user profile on the computer
-$SID = Get-ChildItem 'REGISTRY::HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\ProfileList\' | Select -ExpandProperty Name
+$SID = Get-ChildItem 'REGISTRY::HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\ProfileList\' | Select-Object -ExpandProperty Name
 foreach ($s in $SID) {
     $Prof = Get-ItemProperty -Path "REGISTRY::$s" -Name "ProfileImagePath"
     $User = ($Prof.ProfileImagePath.ToString()).Split('\')[-1]
