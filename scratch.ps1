b866c49016093d43290487a9b247432b1aad22b6
drummermanrob20 <robert@stapleton-family.net>
2022-06-18 09:41:09 -0400

    getting SID with matching username

--- scratch.ps1
+++ scratch.ps1
@@ -1,2 +1,9 @@
 # returns list of folders corresponding to SIDs on a machine
-Get-ChildItem 'REGISTRY::HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\ProfileList\' | Select Name
+# Get-ChildItem 'REGISTRY::HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\ProfileList\' | Select Name
+
+# returns the username (SamAccountName) and corresponding SID for each user profile on the computer
+$SID = Get-ChildItem 'REGISTRY::HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\ProfileList\' | Select -ExpandProperty Name
+foreach ($s in $SID) {
+    Get-ItemProperty -Path "REGISTRY::$s" -Name "ProfileImagePath"
+}
+
